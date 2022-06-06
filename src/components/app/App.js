6d14c986520094b18d738c81d8211175c03b950d
olyao23 <101214018+olyao23@users.noreply.github.com>
2022-06-06 15:13:59 -0200

    Installed react and theme-ui. Initial setup

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,20 +1,18 @@
-import { Box, Button, Text } from "theme-ui";
+import { Box, Text, Button } from 'theme-ui';
 
-function App() {
+export default function App() {
   return (
-    <Box
-      sx={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        gap: 2,
-        mt: 4,
-      }}
-    >
-      <Text>Hello world</Text>
-      <Button>A button</Button>
-    </Box>
+      <Box
+          sx={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              gap: 2,
+              mt: 4,
+          }}
+      >
+          <Text>Hello world</Text>
+          <Button>A button</Button>
+      </Box>
   );
 }
-
-export default App;
